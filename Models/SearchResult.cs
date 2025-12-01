b2de2ed7daf53403b4509544654ec50e9f90769b
Aaron Farrelly <X00203931@mytudublin.ie>
2025-12-01 18:54:04 +0000

    Added models for TV api use

---
+++ Models/SearchResult.cs
@@ -0,0 +1,8 @@
+﻿namespace CA3_WebApp.Models
+{
+    public class SearchResult
+    {
+        public double Score { get; set; }
+        public Show? Show { get; set; }
+    }
+}
---
+++ Models/Show.cs
@@ -0,0 +1,16 @@
+﻿namespace CA3_WebApp.Models
+{
+    public class Show
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public string? Author { get; set; }
+    }
+
+    public class ShowImage
+    {
+        public string? Medium { get; set; }
+        public string? Original { get; set; }
+    }
+}
