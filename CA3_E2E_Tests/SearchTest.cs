fec91106ade1d2f5d2c39b987d21e49123dd33f3
Aaron Farrelly <X00203931@mytudublin.ie>
2025-12-06 19:45:56 +0000

    Added E2E testing, playwright installation, code quality check steps to pipeline. Added additional test

--- CA3_E2E_Tests/SearchTest.cs
+++ CA3_E2E_Tests/SearchTest.cs
@@ -14,7 +14,7 @@ public class SearchTests
         // find tv show search input and search for 'the wire'
         await page.WaitForSelectorAsync("text=Search for a TV show");
         await page.GetByLabel("Search for a TV Show").FillAsync("the wire");
-        await page.GetByRole(AriaRole.Button, new() { Name = "Search" }).ClickAsync();
+        await page.GetByRole(AriaRole.Button, new() {Name = "Search"}).ClickAsync();
 
         // confirm the page loads and shows atleast 1 show
         await page.Locator(".show-card").First.WaitForAsync();
@@ -33,7 +33,7 @@ public class SearchTests
         // find tv show search input and search for 'the wire'
         await page.WaitForSelectorAsync("text=Search for a TV show");
         await page.GetByLabel("Search for a TV Show").FillAsync("the wire");
-        await page.GetByRole(AriaRole.Button, new() { Name = "Search" }).ClickAsync();
+        await page.GetByRole(AriaRole.Button, new() {Name = "Search"}).ClickAsync();
 
         // confirm the page loads and shows atleast 1 show
         await page.Locator(".show-card").First.WaitForAsync();
@@ -41,8 +41,24 @@ public class SearchTests
 
         // wait for details page content
         await page.WaitForSelectorAsync(".show-title");
-
+        // test completed
         Assert.True(true);
     }
 
+    [Fact]
+    public async Task EmptySearch_ShowsValidationMessage()
+    {
+        using var pw = await Playwright.CreateAsync();
+        var browser = await pw.Chromium.LaunchAsync(new() { Headless = true });
+        var page = await browser.NewPageAsync();
+
+        await page.GotoAsync("https://afarrelly90.github.io/CA3_WebApp_X00203931/");
+        await page.WaitForSelectorAsync("text=Search for a TV show");
+        // leave input empty and click Search
+        await page.GetByRole(AriaRole.Button, new() {Name = "Search").ClickAsync();
+        // find error message
+        await page.WaitForSelectorAsync("text=Please enter something to search.");
+        // test completed
+        Assert.True(true);
+    }
 }
